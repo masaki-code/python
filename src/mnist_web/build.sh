0c7f41c2ca6f84cbeb1bc084aee4ab521627a1dc
masaki <masaki@masaki-blog.net>
2020-08-30 14:55:21 +0900

    work

--- src/mnist_web/build.sh
+++ src/mnist_web/build.sh
@@ -3,8 +3,8 @@
 IMAGE=my-keras-mnist_web
 TAG=tmp_`date +"%Y%m%d_%H%M%S"`
 CONTAINER=my-keras-mnist_web-app_$TAG
-HOST_DIR=$PWD/files
-GEST_DIR=/home/keras/src
+# HOST_DIR=$PWD/files
+# GEST_DIR=/home/keras/src
 echo ======================
 echo build
 echo ======================
@@ -18,7 +18,8 @@ docker image ls $IMAGE
 echo ======================
 echo run
 echo ======================
-docker run -dit --name $CONTAINER -p 80:80 -v $HOST_DIR:$GEST_DIR $IMAGE:$TAG
+# docker run -dit --name $CONTAINER -p 80:80 -v $HOST_DIR:$GEST_DIR $IMAGE:$TAG
+docker run -dit --name $CONTAINER -p 80:80 $IMAGE:$TAG
 
 echo ======================
 echo ps
@@ -33,4 +34,4 @@ docker exec -it $CONTAINER /bin/bash
 echo ======================
 echo stop
 echo ======================
-docker stop $CONTAINER 
+docker stop $CONTAINER
---
+++ src/mnist_web/files/php/index.php
@@ -0,0 +1,61 @@
+<?php
+require_once 'Consts.php';
+
+function imgBase64()
+{
+    if (isset($_POST['imgBase64'])) {
+        return $_POST['imgBase64'];
+    }
+    return '';
+}
+
+function save($imgBase64)
+{
+    if ($imgBase64 == '') {
+        return;
+    }
+
+    if ($imgBase64 == Consts::$EMPTY_BASE64) {
+        return;
+    }
+
+    $time = time();
+    $date = date("Ymd", $time);
+    $file_name = 'file/Handwriting_' . $date . '_' . $time . '.png';
+    $decode = base64_decode($imgBase64);
+    file_put_contents($file_name, $decode);
+}
+
+$imgBase64 = imgBase64();
+save($imgBase64);
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<META http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>HTML5のcanvasに手書き文字</title>
+<style type="text/css"></style>
+<link rel="stylesheet" href="Handwriting.css">
+<script type="text/javascript" src="Handwriting.js"></script>
+</head>
+<body onload="mam_draw_init();">
+    <h3>HTML5のcanvasに手書き文字</h3>
+    <div style="border: solid 1px #000000; width: 200px;" id="candiv">
+        <canvas id="can" width="200px" height="200px"></canvas>
+    </div>
+    <br>
+    <input type="button" onClick="clearCan();" value="クリア" style="width: 100; height: 30;" data-inline="true" />
+    <form id="fm" name="fm" method="post" action="/" style="display: inline;">
+        <input type="button" onClick="savePic();" value="サーバーへ保存" style="width: 100; height: 30;" data-inline="true" />
+        <input type="hidden" name="imgBase64" id="imgBase64" value="" data-inline="true" />
+    </form>
+    <br>
+    <?php
+    if ($imgBase64 != '') {
+        echo ('<img src="data:image/png;base64,' . $imgBase64 . '" />');
+    }
+
+    ?>
+</body>
+</html>
